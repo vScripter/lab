2b10162104b802ef665b1dae97d503bb649d1dfc
Kevin Kirkpatrick <kevin@vmotioned.com>
2017-07-27 17:11:04 -0400

    Added script files to manage lab
    Signed-off-by: Kevin Kirkpatrick <kevin@vmotioned.com>

---
+++ aws/ws16-lab/tf-output-stack.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# chmod u+x <file>.sh
+
+: <<'END'
+cd ~/GitHub/Lab/aws/ws16-lab/dc-01
+echo '------------------------ [ws16-lab][Plan] Working on {dc-01}                ------------------------'
+terraform plan
+
+cd ~/GitHub/Lab/aws/ws16-lab/core-member-servers
+echo '------------------------ [ws16-lab][Plan] Working on {Core-Member-Servers}  ------------------------'
+terraform plan
+
+cd ~/GitHub/Lab/aws/ws16-lab/member-servers
+echo '------------------------ [ws16-lab][Plan] Working on {Member-Servers}       ------------------------'
+terraform plan
+
+
+
+cd dc-01
+echo '------------------------ [ws16-lab][Apply] Working on {dc-01}                ------------------------'
+terraform apply
+
+cd ../core-member-servers
+echo '------------------------ [ws16-lab][Apply] Working on {Core-Member-Servers}  ------------------------'
+terraform apply
+
+cd ../member-servers
+echo '------------------------ [ws16-lab][Apply] Working on {Member-Servers}       ------------------------'
+terraform apply
+END
+
+cd dc-01
+echo '------------------------ [ws16-lab][Output] {dc-01}               ------------------------'
+terraform output
+
+cd ../core-member-servers
+echo '------------------------ [ws16-lab][Output] {core-member-servers} ------------------------'
+terraform output
+
+cd ../member-servers
+echo '------------------------ [ws16-lab][Output] {member-servers}      ------------------------'
+terraform output
