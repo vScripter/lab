d32c3fc2e01a2ca59bab064345ebb53ac552ea99
Kevin Kirkpatrick <kevin@vmotioned.com>
2017-07-27 17:16:17 -0400

    Updated destroy script
    Signed-off-by: Kevin Kirkpatrick <kevin@vmotioned.com>

--- aws/ws16-lab/tf-destroy-stack.sh
+++ aws/ws16-lab/tf-destroy-stack.sh
@@ -33,12 +33,12 @@ END
 
 cd dc-01
 echo '------------------------ [ws16-lab][DESTROY] {dc-01}               ------------------------'
-terraform destroy -force
+terraform destroy -force -var 'admin_password=foo'
 
 cd ../core-member-servers
 echo '------------------------ [ws16-lab][DESTROY] {core-member-servers} ------------------------'
-terraform destroy -force
+terraform destroy -force -var 'admin_password=foo'
 
 cd ../member-servers
 echo '------------------------ [ws16-lab][DESTROY] {member-servers}      ------------------------'
-terraform destroy -force
+terraform destroy -force -var 'admin_password=foo'
