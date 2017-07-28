ba42a68cdf0a4ecfb1093d6acc42b95954ceb3bd
Kevin Kirkpatrick <kevin@vmotioned.com>
2017-07-28 00:56:29 -0400

    Reverted back to using in-line user_data
    Signed-off-by: Kevin Kirkpatrick <kevin@vmotioned.com>

--- aws/modules/ws16-core/main.tf
+++ aws/modules/ws16-core/main.tf
@@ -45,14 +45,14 @@ data "aws_ami" "ws16_core" {
   }
 }
 
-data "template_file" "user_data" {
+/*data "template_file" "user_data" {
   template = "${file("${path.module}/user-data.txt")}"
 
   vars {
     password       = "${var.admin_password}"
-    hostname       = "${var.instance_name}-${count.index}"
+    #hostname       = "${var.instance_name}-${count.index}"
   }
-}
+}*/
 
 resource "aws_instance" "ws16_core" {
   count                  = "${var.instance_count}"
@@ -61,8 +61,8 @@ resource "aws_instance" "ws16_core" {
   key_name               = "${var.aws_key_pair}"
   subnet_id              = "${data.terraform_remote_state.vpc.subnet_10-10-1-0_24_id}"
   vpc_security_group_ids = ["${data.terraform_remote_state.vpc.instance_sg}"]
-  user_data              = "${data.template_file.user_data.rendered}"
-  /*user_data              = <<EOF
+  #user_data              = "${data.template_file.user_data.rendered}"
+  user_data              = <<EOF
 <script>
   winrm quickconfig -q & winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"} & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"}
 </script>
@@ -70,10 +70,15 @@ resource "aws_instance" "ws16_core" {
   netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow
   $admin = [adsi]("WinNT://./administrator, user")
   $admin.psbase.invoke("SetPassword", "${var.admin_password}")
-  Rename-Computer -NewName "${var.instance_name}-${count.index}" -Restart -Confirm:$False
+  Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+  Install-Module xActiveDirectory -Force
+  Install-Module xComputerManagement -Force
+  Get-NetFirewallRule -Name *icmp4-erq*|Enable-NetFirewallRule
+  Get-NetFirewallRule -Name *fps-smb*|Enable-NetFirewallRule
+  Rename-Computer -NewName "${var.instance_name}-${count.index}" -Force -Restart
 </powershell>
 EOF
-*/
+
   tags {
     Name = "${var.instance_name}-${count.index}"
   }
