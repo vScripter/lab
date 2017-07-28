f57fd1cd5211269fece294a9b784b9cfd0a4c93a
Kevin Kirkpatrick <kevin@vmotioned.com>
2017-07-28 00:54:45 -0400

    Added password var to stack scripts
    Signed-off-by: Kevin Kirkpatrick <kevin@vmotioned.com>

--- aws/ws16-lab/tf-apply-stack.sh
+++ aws/ws16-lab/tf-apply-stack.sh
@@ -19,15 +19,15 @@ END
 
 cd dc-01
 echo '------------------------ [ws16-lab][Apply] Working on {dc-01}                ------------------------'
-terraform apply
+terraform apply -var 'admin_password=VMware1!'
 
 cd ../core-member-servers
 echo '------------------------ [ws16-lab][Apply] Working on {Core-Member-Servers}  ------------------------'
-terraform apply
+terraform apply -var 'admin_password=VMware1!'
 
 cd ../member-servers
 echo '------------------------ [ws16-lab][Apply] Working on {Member-Servers}       ------------------------'
-terraform apply
+terraform apply -var 'admin_password=VMware1!'
 
 
 
--- aws/ws16-lab/tf-plan-stack.sh
+++ aws/ws16-lab/tf-plan-stack.sh
@@ -4,15 +4,15 @@
 
 cd dc-01
 echo '------------------------ [ws16-lab][Plan] Working on {dc-01}                ------------------------'
-terraform plan
+terraform plan -var 'admin_password=VMware1!'
 
 cd ../core-member-servers
 echo '------------------------ [ws16-lab][Plan] Working on {Core-Member-Servers}  ------------------------'
-terraform plan
+terraform plan -var 'admin_password=VMware1!'
 
 cd ../member-servers
 echo '------------------------ [ws16-lab][Plan] Working on {Member-Servers}       ------------------------'
-terraform plan
+terraform plan -var 'admin_password=VMware1!'
 
 
 : <<'END'
