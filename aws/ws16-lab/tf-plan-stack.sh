ddc22a59bb1f017d3fa565594e3745fc529bc0f2
Kevin Kirkpatrick <kevin@nullzero.io>
2017-10-05 14:54:13 -0400

    Removed confusing comments from init scripts
    Signed-off-by: Kevin Kirkpatrick <kevin@nullzero.io>

--- aws/ws16-lab/tf-plan-stack.sh
+++ aws/ws16-lab/tf-plan-stack.sh
@@ -13,18 +13,3 @@ terraform plan -var 'admin_password=VMware1!'
 cd ../member-servers
 echo '------------------------ [ws16-lab][Plan] Working on {Member-Servers}       ------------------------'
 terraform plan -var 'admin_password=VMware1!'
-
-
-: <<'END'
-cd ~/GitHub/Lab/aws/ws16-lab/dc-01
-echo '------------------------ [ws16-lab][Apply] Working on {dc-01}                ------------------------'
-terraform apply
-
-cd ~/GitHub/Lab/aws/ws16-lab/core-member-servers
-echo '------------------------ [ws16-lab][Apply] Working on {Core-Member-Servers}  ------------------------'
-terraform apply
-
-cd ~/GitHub/Lab/aws/ws16-lab/member-servers
-echo '------------------------ [ws16-lab][Apply] Working on {Member-Servers}       ------------------------'
-terraform apply
-END
--- aws/ws16-lab/tf-show-stack.sh
+++ aws/ws16-lab/tf-show-stack.sh
@@ -2,34 +2,6 @@
 
 # chmod u+x <file>.sh
 
-: <<'END'
-cd ~/GitHub/Lab/aws/ws16-lab/dc-01
-echo '------------------------ [ws16-lab][Plan] Working on {dc-01}                ------------------------'
-terraform plan
-
-cd ~/GitHub/Lab/aws/ws16-lab/core-member-servers
-echo '------------------------ [ws16-lab][Plan] Working on {Core-Member-Servers}  ------------------------'
-terraform plan
-
-cd ~/GitHub/Lab/aws/ws16-lab/member-servers
-echo '------------------------ [ws16-lab][Plan] Working on {Member-Servers}       ------------------------'
-terraform plan
-
-
-
-cd dc-01
-echo '------------------------ [ws16-lab][Apply] Working on {dc-01}                ------------------------'
-terraform apply
-
-cd ../core-member-servers
-echo '------------------------ [ws16-lab][Apply] Working on {Core-Member-Servers}  ------------------------'
-terraform apply
-
-cd ../member-servers
-echo '------------------------ [ws16-lab][Apply] Working on {Member-Servers}       ------------------------'
-terraform apply
-END
-
 cd dc-01
 echo '------------------------ [ws16-lab][Show] {dc-01}               ------------------------'
 terraform show
