ca63e63eb7f8f77b016d6489cdbb7bb45a05ef95
Kevin Kirkpatrick <kevin@nullzero.io>
2017-11-21 17:37:28 -0500

    DSC Lab :: Allow RDP FW Rules
    Signed-off-by: Kevin Kirkpatrick <kevin@nullzero.io>

--- powershell-dsc/Skynet-Member-Server.ps1
+++ powershell-dsc/Skynet-Member-Server.ps1
@@ -51,6 +51,7 @@ Install-Module xRemoteDesktopAdmin -Force
 # Setup FW rules to allow ICMP and SMB
 Get-NetFirewallRule -Name *icmp4-erq*|Enable-NetFirewallRule
 Get-NetFirewallRule -Name *fps-smb*|Enable-NetFirewallRule
+Get-NetFirewallRule -Name *remotedesktop*|Enable-NetFirewallRule
 
 # Start init
 Set-Location C:\
